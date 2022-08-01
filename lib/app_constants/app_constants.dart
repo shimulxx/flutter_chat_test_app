2a28346c997a707397eea31fdc2ec53797636369
Shimul <shimul6680@gmail.com>
2022-08-01 23:28:12 +0600

    complete chat list screen......

---
+++ lib/app_constants/app_constants.dart
@@ -0,0 +1,4 @@
+//default avatar image
+const kAvatarDefaultPhotoUrl = 'https://www.seekpng.com/png/full/428-4287240_no-avatar-user-circle-icon-png.png';
+//default avatar image (local)
+const kDefaultLocalAvatarPhoto = 'assets/images/pngs/avatar_default.png';
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_test_app/app_constants/app_constants.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,104 +14,103 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorScheme: const ColorScheme.dark(),),
+      home: const ChatListScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class ChatListScreen extends StatelessWidget {
+  const ChatListScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
+      appBar: AppBar(title: const Text('Chat List Screen'),),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        elevation: 5,
+        splashColor: Theme.of(context).splashColor,
+        backgroundColor: const Color(0xff141414), //Theme.of(context).backgroundColor,
+        child: const Icon(Icons.add, color: Colors.white,),
+        onPressed: (){},
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index){
+          return ChatListItem(
+            onPressed: (){
+              print('clicked');
+            },
+            imageUrl: kAvatarDefaultPhotoUrl,
+            name: 'Faruq New',
+            lastChat: 'Rana Vai',
+            lastTime: '7:22 PM',
+            isDelivered: true,
+          );
+        },
+
+      ),
     );
   }
 }
+
+class ChatListItem extends StatelessWidget {
+  const ChatListItem({
+    Key? key,
+    required this.name,
+    required this.imageUrl,
+    required this.isDelivered,
+    required this.lastChat,
+    required this.lastTime,
+    required this.onPressed,
+  }) : super(key: key);
+
+  final String name, imageUrl, lastChat, lastTime;
+  final bool isDelivered;
+  final Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                CachedNetworkImage(
+                  imageUrl: kAvatarDefaultPhotoUrl,
+                  height: 50,
+                  width: 50,
+                  errorWidget: (c, u, e) => Image.asset(imageUrl, height: 50, width: 50,),
+                  placeholder: (c, s) => const Padding(padding: EdgeInsets.all(10), child: CircularProgressIndicator()),
+                ),
+                const SizedBox(width: 10,),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(name, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                    SizedBox(height: 5,),
+                    Row(
+                      children: [
+                        for(var i = 1; i <= (isDelivered ? 2 : 1); ++i) Transform.scale(scale: 2, child: const Icon(Icons.check, size: 6, )),
+                        const SizedBox(width: 5,),
+                        Text(lastChat, style: const TextStyle(fontSize: 11),)
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+            Text(lastTime)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
