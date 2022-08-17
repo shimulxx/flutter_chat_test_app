11670b0058530284a37ca5b3da13cfa32d34a17e
classtune <shimul6680@gmail.com>
2022-08-17 11:47:35 +0600

    keyboard dismiss minor bug fixed....

--- lib/screen/chat_details_screen/details_screen.dart
+++ lib/screen/chat_details_screen/details_screen.dart
@@ -1,18 +1,14 @@
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_chat_test_app/core/constants.dart';
 import 'package:flutter_chat_test_app/screen/chat_details_screen/controller/chat_room_details_cubit.dart';
 import 'package:flutter_chat_test_app/screen/chat_details_screen/controller/chat_room_details_state.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import '../../date_time/date_time_use_cases.dart';
 import '../../injection_work.dart';
-import '../chat_list_screen/data_model/chat_item_data.dart';
 import 'chat_text_field.dart';
 import 'data_model/chat_detail_item_data.dart';
 import 'inner_widget/app_bar.dart';
 import 'inner_widget/chat_details_screen_list.dart';
-import 'inner_widget/inner_widget/chat_details_list_item.dart';
 
 class ChatDetailsScreenBody extends StatelessWidget {
   const ChatDetailsScreenBody({
@@ -26,15 +22,9 @@ class ChatDetailsScreenBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final bloc = context.read<ChatRoomDetailsCubit>();
-    // final list = <ChatDetailItemData>[];
-    // for(var i = 1; i <= 20; ++i){
-    //   list.add(ChatDetailItemData(
-    //     chatText: 'The name of our country is Bangladesh. This is a very big country. But this is a very beautiful country.',
-    //     isDelivered: i & 1 == 1,
-    //     sendTime: '5:25 PM',
-    //     userId: i & 1 == 1 ? 123.toString() : curUserId,
-    //   ));
-    // }
+    final bool isKeyboardVisible = KeyboardVisibilityProvider.isKeyboardVisible(context);
+    //this visible flag is not necessary since internal controller is already controlled with rebuild
+    print(isKeyboardVisible);
     return Scaffold(
       appBar: AppBarChatDetailsScreen(name: name, imageUrl: imageUrl),
       body: Column(
